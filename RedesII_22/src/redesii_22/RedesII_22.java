ba17b6874ff6276efd800870c32c6d5a2097190f
dinhujr <c.souza0804@gmail.com>
2016-01-07 00:24:03 -0200

    first bini

---
+++ RedesII_22/src/redesii_22/RedesII_22.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redesii_22;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Claudio
+ */
+public class RedesII_22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in          = new Scanner(System.in);
+        String message      = in.nextLine();
+        byte[] byteMessage  = message.getBytes();
+        try{
+            
+            RandomAccessFile raf = new RandomAccessFile(new File("Message.meta"), "rw");
+            
+            for(int i = 0 ; i < byteMessage.length; i ++)
+            {
+                raf.writeByte( byteMessage[i] );
+            }
+            
+            raf.close();
+            
+            raf = new RandomAccessFile( new File("Message.meta") , "r");
+            
+            int columnParity    = 0;
+            int lineParity      = 0;
+            
+            for(int i = 0; i < raf.length(); i ++)
+            {
+                byte temp       = raf.readByte();
+                columnParity    = columnParity ^ temp;
+                lineParity      = lineParity + temp;
+                String tempStr  = "" + temp;
+                System.out.println(new BigInteger(tempStr.getBytes()).toString(2));
+            }
+            String tempOp       = "" + columnParity;
+            System.out.println("Column " + (new BigInteger( tempOp.getBytes()).toString(2)) );
+            tempOp              = "" + lineParity;
+            System.out.println("Line " + (new BigInteger( tempOp.getBytes()).toString(2)) );
+            raf.close();
+        }catch(Exception e) {
+            
+        }
+    }
+    
+}
