0d1256165770e9bbad1a3de7cfecdb6222d06faa
vasconcellosv <viniciusdeck@gmail.com>
2016-01-19 21:48:38 -0200

    Clean class

--- RedesII_22/src/br/uff/redesIIparity/model/ConversationModel_Receiver.java
+++ RedesII_22/src/br/uff/redesIIparity/model/ConversationModel_Receiver.java
@@ -24,17 +24,15 @@ public class ConversationModel_Receiver {
 
     private final int MINELEMENTSNUMBER = 8;
 
-    private final int ELEMENTSINPARITYARRAY = 10;
-
     private final ReceiverMessage sender = new ReceiverMessage();
 
     private ConversationPanel_Receiver panel;
 
-    public byte[] getMessage(String path) throws Exception {
+    public byte[] getMessage(File file) throws Exception {
         //return workFile(path);
         RandomAccessFile raf = null;
         
-        File file = new File(path);
+        //File file = new File(path);
         
         try {
             raf = new RandomAccessFile( file, "r" );
@@ -62,159 +60,6 @@ public class ConversationModel_Receiver {
         
         sender.flushMessage();
         return null;
-    }
-
-    public byte [] workFile(File selectedFile) throws IOException
-    {
-        this.sender.createCloneFileName ( selectedFile.getName() );
-        
-        RandomAccessFile raf = null;
-        
-        try {
-            raf = new RandomAccessFile( selectedFile, "r" );
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ConversationModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        int count = 0;
-        
-        byte[] byteBuffer = getNewByteArray();
-        
-        int iteraction = 1;
-        
-        if( raf != null )
-        {
-            long length = raf.length();
-            
-            if( length < 8 )
-            {
-                iteraction = (int)length;
-                for (int i = 0; i < length; i++) {
-                    byteBuffer[i] = raf.readByte();
-                    
-                }
-            }
-            else
-            {
-                while( count < length )
-                {
-                    byteBuffer[iteraction - 1] = raf.readByte();
-
-                    if ( iteraction == 8 )
-                    {
-
-                        //sender.writeBuffer( getByteArrayWithParity(byteBuffer) );
-
-                        iteraction = 1;
-                        byteBuffer = getNewByteArray();
-                    }
-
-                    iteraction ++;
-                    count ++;
-                }
-            }
-            
-            if( iteraction < 8 )
-            {
-                int iteractionRemaining = 8 - iteraction + 1;
-                for (int i = 0; i < iteractionRemaining; i++) {
-                    byteBuffer[i+iteraction-1] = 0;
-                }
-                //sender.writeBuffer( getByteArrayWithParity(byteBuffer) );
-            }
-            
-            
-            
-            //sender.closeFile();
-            
-            raf.close();
-            
-            return byteBuffer;
-        }
-        return null;
-    }
-
-    public boolean workMessage(String message) throws Exception {
-        char[] messageArray = message.toCharArray();
-
-        int elementsRemaining = MINELEMENTSNUMBER - messageArray.length;
-
-        if (elementsRemaining > 0) {
-            char[] tempArray = new char[MINELEMENTSNUMBER];
-
-            int countStopped = 0;
-
-            for (int i = 0; i < messageArray.length; i++) {
-                tempArray[i] = messageArray[i];
-                countStopped++;
-            }
-
-            for (int i = 0; i < elementsRemaining; i++) {
-                tempArray[i + countStopped] = '0';
-            }
-
-            messageArray = tempArray;
-
-        }
-
-        int arrayPosition = 0;
-
-        byte[] messagesInBytes = getNewByteArray();
-
-        int tempArrayPosition = 0;
-
-        int iteration = 1;
-
-        while (arrayPosition < messageArray.length) {
-            if (iteration % 8 == 0) {
-                tempArrayPosition = 0;
-
-                boolean wasSent = sender.sendMessage(getByteArrayWithParity(messagesInBytes), "");
-
-                messagesInBytes = getNewByteArray();
-
-                if (!wasSent) {
-                    throw new Exception("Message was not sent");
-                }
-
-            }
-
-            messagesInBytes[tempArrayPosition] = (byte) messageArray[arrayPosition];
-
-            iteration++;
-            tempArrayPosition++;
-            arrayPosition++;
-
-        }
-
-        sender.flushMessage();
-        return true;
-
-    }
-
-    private byte[] getNewByteArray() {
-        return new byte[MINELEMENTSNUMBER];
-    }
-
-    private byte[] getByteArrayWithParity(byte[] messagesInBytes) {
-        byte[] arrayWithParity = new byte[ELEMENTSINPARITYARRAY];
-
-        byte lineParity = getLineParity(messagesInBytes);
-
-        byte columnParity = getColumnParity(messagesInBytes);
-
-        arrayWithParity[0] = columnParity;
-        arrayWithParity[1] = lineParity;
-
-        fillArray(messagesInBytes, arrayWithParity);
-
-        return arrayWithParity;
-    }
-
-    private void fillArray(byte[] messagesInBytes, byte[] arrayWithParity) {
-        for (int i = 0; i < messagesInBytes.length; i++) {
-            arrayWithParity[i + 2] = messagesInBytes[i];
-        }
     }
 
     private byte getColumnParity(byte[] messagesInBytes) {
